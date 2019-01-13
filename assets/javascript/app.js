bbaf4e57a5b1105914caaacb56368ad4def84298
Kendall Davis <kendallsdavis@gmail.com>
2019-01-13 13:35:02 -0500

    rough working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,102 +1,89 @@
 
 $(document).ready(function() { 
 
-var QuizQuestions = [{
-    question: "Which bird has eyes that are larger than it's brain?",
-    choices: ["Blue Bird", "Owl", "Ostrich", "Canary" ],
-    validAnswer: 2
-}, {
-    question:"What is the only bird known to fly backwards?",
-    choices: ["Hummingbird", "Swan", "Parakeet", "Finch"],
-    validAnswer: 0    
-}, {
-    question:"Which of the 5 senses do dolphins not possess? ",
-    choices: ["Sound", "Touch", "Smell", "Sight"],
-    validAnswer: 2        
-}, {
-    question:"What is a group of whales called? ",
-    choices: ["A lark", "A squad", "A corpus", "A pod"],
-    validAnswer: 3       
-}, {
-    question:"What is the proper term for a group of parrots?",
-    choices: ["A pandemonium", "A flight", "A parliament", "A swarm"],
-    validAnswer: 0
-}, {
-    question:"What is the national animal of Scotland? ",
-    choices: ["The gopher", "The cat", "The unicorn", "The bison"],
-    validAnswer: 2   
-}];
 
-var number = 10;
-var guesses = []
+        var number = 10;
+        var intervalId;
+        var correct = 0;
+        var incorrect = 0;
+        var guesses = ["", "", "", "", "", ""];
+        var answers = ["Ostrich", "Hummingbird", "Smell", "A pod", "A pandemonium", "The unicorn"];
+    
+        $("#done").on("click", stop);    
+        $("#start").on("click", run);
 
-Interval = setInterval(decrement, 1000)
+// Define functions for run, reset, stop, decrement, and guessing
+        function run() {
+          status = "started";
+          clearInterval(intervalId);
+          intervalId = setInterval(decrement, 1000);
+          guessing();
+          }
 
-function decrement() {
-    if(number > 1){
-    //  Decrease number by one.
-    number = number - 1;
-    //  Show the number in the #timer tag.
-    $("#timer").html("<h2>" + number + " seconds remaining" + "</h2>");
-    } else { 
-        number = "Time's Up!!";
-        $("#timer").html(number);
-    }
-}
+        function reset () {
+            number = 5;
+            correct = 0;
+            incorrect = 0;
+            guesses = ["", "", "", "", "", ""];
+            run();
+        }
+
+        function stop() {
+            clearInterval(intervalId);
+            $("input[type=radio]").attr('disabled', true);
+            // function compare() {
+                for(var i = 0; i < 6; i++){
+                    if(guesses[i] === answers[i]){
+                    correct++
+                    } else {
+                        incorrect++
+                    };
+                        console.log(correct);
+                        console.log(incorrect);
+                        number = 10;
+                    $("#questionbox").html("");
+                    $("#questionbox").html("<h2>All Done!<br/></h2> < <h3>Correct Answers:</h3><br/>  " + correct + "<h3> Incorrect Answers: </h3> " + incorrect);
+                };
+            };
+          
+
+        function decrement() {    
+          number--;
+          $("#timer").html("<h2>" + number + "</h2>");
+            if (number === 0) {
+                stop();
+                console.log("Time Up!");
+                status = "done"
+          }
+        }
+
+        function guessing(){
+            $("#questions").click(function(){    
+                guesses[0] = ($('input[name="eyes"]:radio:checked').val());
+                guesses[1] = ($('input[name="backwards"]:radio:checked').val());
+                guesses[2] = ($('input[name="dolphins"]:radio:checked').val());
+                guesses[3] = ($('input[name="whales"]:radio:checked').val());
+                guesses[4] = ($('input[name="parrots"]:radio:checked').val());
+                guesses[5] = ($('input[name="scotland"]:radio:checked').val());
+                console.log(guesses);
+                });
+            };
 
 
-// var $Question1 = $('input[name="eyes"]');
-//         // $Question1.change(function() {
-//         //     var selected = $Question1.filter(':checked');
-//         //     });
-//             // Output the value of the checked radio
-//             console.log($Question1.filter(':checked').val());
 
-        
-
-
-
-$(function(){
-    $("#Q1").click(function(){      
-       guess1 = ($('input[name="eyes"]:radio:checked').val());
-       console.log(guess1);
-   });
-  });
-
-  $(function(){
-    $("#Q2").click(function(){      
-       guess2 = ($('input[name="backwards"]:radio:checked').val());
-       console.log(guess2);
-   });
-  });
-
-  $(function(){
-    $("#Q3").click(function(){      
-       guess3 = ($('input[name="dolphins"]:radio:checked').val());
-       console.log(guess3);
-   });
-  });
-
-  $(function(){
-    $("#Q4").click(function(){      
-       guess4 = ($('input[name="whales"]:radio:checked').val());
-       console.log(guess4);
-   });
-  });
-
-  $(function(){
-    $("#Q5").click(function(){      
-       guess5 = ($('input[name="parrots"]:radio:checked').val());
-       console.log(guess5);
-   });
-  });
-
-  $(function(){
-    $("#Q6").click(function(){      
-       guess6 = ($('input[name="scotland"]:radio:checked').val());
-       console.log(guess6);
-   });
-  });
-
-        });
- 
+        if(status === "started"){
+            $(function(){
+            $("#questions").click(function(){    
+                guesses[0] = ($('input[name="eyes"]:radio:checked').val());
+                guesses[1] = ($('input[name="backwards"]:radio:checked').val());
+                guesses[2] = ($('input[name="dolphins"]:radio:checked').val());
+                guesses[3] = ($('input[name="whales"]:radio:checked').val());
+                guesses[4] = ($('input[name="parrots"]:radio:checked').val());
+                guesses[5] = ($('input[name="scotland"]:radio:checked').val());
+                console.log(guesses);
+                });
+            });
+            };
+        console.log(correct);
+        console.log(incorrect);
+    })
